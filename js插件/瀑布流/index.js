d4c154dbfb0dce9540442ece0b2309cb4b4b3b4f
Abby Shaw <reading.bear36@gmail.com>
2016-01-18 16:39:54 +0800

    添加文件 瀑布流

---
+++ js插件/瀑布流/index.js
@@ -0,0 +1,124 @@
+window.onload = function() {
+	var oImgList = document.getElementById('imgList');
+	var oLoader = document.getElementById('loader');
+	var iWidth = 200;	//图片宽度
+	var iSpace = 10;	//图片间隔
+	var iOuterWidth = iWidth + iSpace;		//列实际宽度
+	var iCell = 0;		//列数
+	var arrL = [];
+	var arrT = [];
+	var bBtn = true;
+
+	//计算列数
+	function getCell() {
+		iCell = Math.floor(document.documentElement.clientWidth/iOuterWidth);
+		oImgList.style.width = iCell * iOuterWidth - iSpace + "px";
+	}
+	getCell();
+
+	for (var i = 0; i < iCell; i++) {
+		arrL[i] = iOuterWidth * i;
+		arrT[i] = 0;
+	};
+
+	function getImgRequest() {
+		if (!bBtn) {
+			return;
+		};
+		bBtn = false;
+		oLoader.style.display = "block";
+		var request = createRequest();
+		if (request == null) {
+			alert("Unable to create request!");
+		} else {
+			var url = "index.php";
+			request.onreadystatechange = showImg;
+			request.open("GET",url,true);
+			request.send(null);
+		}
+	}
+	getImgRequest();
+
+	function showImg() {
+		if (request.readyState == 4 && request.status == 200) {
+			var data = JSON.parse(request.responseText);
+			for (var i in data) {
+				var obj = data[i];
+				var oImg = document.createElement("img");
+				var iHeight = iWidth/obj['width'] * obj['height'];
+				oImg.style.cssText = "width:" + iWidth + "px;height:" + iHeight + "px";
+				oImg.src = obj["src"];
+
+				var _index = getMin();
+				oImg.style.cssText += "left:" + arrL[_index] + "px;top:" + arrT[_index] + "px";
+
+				arrT[_index] += iHeight + 10;
+
+				oImgList.appendChild(oImg);
+			};
+			oLoader.style.display = "none";
+			bBtn = true;
+		};
+	}
+
+	window.onscroll = function() {
+		if (arrT[getMin()] < document.documentElement.clientHeight + document.body.scrollTop) {
+			getImgRequest();
+		};
+	}
+
+	function getMin() {
+		var v = arrT[0];
+		var _index = 0;
+
+		for (var i = 0; i < arrT.length; i++) {
+			if (arrT[i] < v) {
+				v = arrT[i];
+				_index = i;
+			};
+		};
+
+		return _index;
+	}
+
+	window.onresize = function() {
+		var oldCell = iCell;
+		getCell();
+		if (iCell == oldCell) {
+			return;
+		};
+		arrT = [];
+		arrL = [];
+		for (var i = 0; i < iCell; i++) {
+			arrT[i] = 0;
+			arrL[i] = i * iOuterWidth;
+		};
+		var oImgs = oImgList.getElementsByTagName('img');
+		for (var i in oImgs) {
+			if (typeof oImgs[i] != "object") {
+				continue;
+			};
+			var _index = getMin();
+			oImgs[i].style.left = arrL[_index] + "px";
+			oImgs[i].style.top = arrT[_index] + "px";
+			arrT[_index] += oImgs[i].offsetHeight + 10;
+		};
+	};
+}
+
+function createRequest() {
+	try {
+		request = new XMLHttpRequest();
+	} catch (tryMS) {
+		try {
+			request = new ActiveXObject("Msxml2.XMLHTTP");
+		} catch (otherMS) {
+			try {
+				request = new ActiveXObject("Microsoft.XMLHTTP");
+			} catch (failed) {
+				request = null;
+			}
+		}
+	}
+	return request;
+}
---
+++ js插件/瀑布流/index.php
@@ -0,0 +1,36 @@
+<?php
+	$arr = array(
+		array("src" => "img/1.jpg", "width" => "960", "height" => "854"),
+		array("src" => "img/2.jpg", "width" => "1600", "height" => "900"),
+		array("src" => "img/3.jpg", "width" => "1440", "height" => "900"),
+		array("src" => "img/4.jpg", "width" => "1280", "height" => "960"),
+		array("src" => "img/5.jpg", "width" => "2938", "height" => "2203"),
+		array("src" => "img/6.jpg", "width" => "1350", "height" => "1998"),
+		array("src" => "img/7.jpg", "width" => "1200", "height" => "1698"),
+		array("src" => "img/8.jpg", "width" => "2440", "height" => "3370"),
+		array("src" => "img/9.jpg", "width" => "1074", "height" => "1518"),
+		array("src" => "img/10.jpg", "width" => "1024", "height" => "640"),
+		array("src" => "img/1.jpg", "width" => "960", "height" => "854"),
+		array("src" => "img/2.jpg", "width" => "1600", "height" => "900"),
+		array("src" => "img/3.jpg", "width" => "1440", "height" => "900"),
+		array("src" => "img/4.jpg", "width" => "1280", "height" => "960"),
+		array("src" => "img/5.jpg", "width" => "2938", "height" => "2203"),
+		array("src" => "img/6.jpg", "width" => "1350", "height" => "1998"),
+		array("src" => "img/7.jpg", "width" => "1200", "height" => "1698"),
+		array("src" => "img/8.jpg", "width" => "2440", "height" => "3370"),
+		array("src" => "img/9.jpg", "width" => "1074", "height" => "1518"),
+		array("src" => "img/10.jpg", "width" => "1024", "height" => "640"),
+		array("src" => "img/1.jpg", "width" => "960", "height" => "854"),
+		array("src" => "img/2.jpg", "width" => "1600", "height" => "900"),
+		array("src" => "img/3.jpg", "width" => "1440", "height" => "900"),
+		array("src" => "img/4.jpg", "width" => "1280", "height" => "960"),
+		array("src" => "img/5.jpg", "width" => "2938", "height" => "2203"),
+		array("src" => "img/6.jpg", "width" => "1350", "height" => "1998"),
+		array("src" => "img/7.jpg", "width" => "1200", "height" => "1698"),
+		array("src" => "img/8.jpg", "width" => "2440", "height" => "3370"),
+		array("src" => "img/9.jpg", "width" => "1074", "height" => "1518"),
+		array("src" => "img/10.jpg", "width" => "1024", "height" => "640")
+	);
+	$data = json_encode($arr);
+	print $data;
+?>
